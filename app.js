a36f6fffa04aec56b53027f5217a8aa5b1ce1fef
Tim Cameron Ryan <id@timryan.org>
2013-01-22 16:12:30 -0500

    First change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 
 // Create the route for the path / and send a creepy response.
 app.get('/', function (req, res) {
-  res.send('<h1>Hello ' + (process.env.USER || 'Olin.js') + '!</h1>');
+  res.send('<h1><marquee>I can see you, ' + (process.env.USER || 'Olin.js') + '!</h1>');
 });
 
 // Start serving webpages.
